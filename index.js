cd56e4b376d04a1e315422cb06fc056b91b522e3
Luke Poirrier <lukepoirrier5@gmail.com>
2021-08-25 19:49:39 -0400

    intialize npm app. build out data template for team

---
+++ index.js
@@ -0,0 +1,18 @@
+const render = require("./src/page-template.js");
+
+const team = [
+      {
+            name: "Luke",
+            id: "1",
+            email: "luke@lukepoirrier.com",
+            role: "manager"
+      },
+      {
+            name: "Wade",
+            id: "2",
+            email: "luke@lukepoirrier.com",
+            role: "engineer"
+      },
+];
+
+console.log(render);
