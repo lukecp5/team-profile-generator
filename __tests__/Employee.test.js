edd3536a435bbd6b729a6a42329907a435458016
Luke Poirrier <lukepoirrier5@gmail.com>
2021-08-25 21:20:34 -0400

    Create Employee test. Add module export to Manager module. Update page-template with object instantiations.

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,15 @@
+// 
+
+const Employee = require('../lib/Employee');
+
+describe('Employee', () => {
+      test('should create employee instance', () => {
+            const e= new Employee();
+            expect(typeof e).toBe('object');
+      });
+
+      test()
+})
+
+
+// test("Can we instantiate an employee object", function(){})
