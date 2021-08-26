97a5080880fdfc8f1c77458c5850caeeca6c762e
Luke Poirrier <lukepoirrier5@gmail.com>
2021-08-25 21:19:28 -0400

    Import Employee Classes to index.js. Add jest to test scripts

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-/*
-Roles: 
-
-Manager: 
-
-*/
+const Employee = require('./lib/Employee')
+const Intern = require('./lib/Intern')
+const Engineer = require('./lib/Engineer')
+const Manager = require('./lib/Manager')
 
 const render = require("./src/page-template.js");
 const fs = require("fs");
@@ -12,9 +10,9 @@ const path = require("path");
 const OUTPUT_DIR = path.resolve(__dirname, "dist")
 const outputPath = path.join(OUTPUT_DIR, "index.html");
 
-new Manager({"Luke"}, 1, "luke@lukepoirrier.com");
-new Engineer({"Luke 2"}, 2, "lp@lukepoirrier.com");
-new Intern({"Luke - Intern"}, 3, "lpintern@lukepoirrier.com");
+new Manager("Luke", 1, "luke@lukepoirrier.com", "32952345");
+new Engineer("Luke 2", 2, "lp@lukepoirrier.com", "3295");
+new Intern("Luke - Intern", 3, "lpintern@lukepoirrier.com", "329523");
 
 fs.writeFileSync(outputPath, render(team), "utf8");
 console.log(render(team));
