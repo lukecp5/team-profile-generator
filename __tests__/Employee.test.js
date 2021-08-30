fc7bf220d44e817b46de99994940742c6885f50c
Luke Poirrier <lukepoirrier5@gmail.com>
2021-08-30 17:41:19 -0400

    Add getRole() test to Employee Class

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -7,6 +7,10 @@ describe('Employee', () => {
             const employee= new Employee();
             expect(typeof employee).toBe('object');
       }),
+      test('when called, getRole() should return a string "Employee"', () => {
+            const employee = new Employee("Test", 1234, "test@test.com");
+            expect(employee.getRole()).toBe('Employee');
+      })
       test('should create employee instance with the name property supplied at instantiation', () => {
             const employeeName = "name test";
             const employee = new Employee(employeeName);
