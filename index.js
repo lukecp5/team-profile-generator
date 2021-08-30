753785bad26aab2b43a92ff2b7a29767a8d6cf67
Luke Poirrier <lukepoirrier5@gmail.com>
2021-08-30 17:18:20 -0400

    Add modules to handle Email, Phone, and Alphabetical verification to inquirer prompts

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ const Engineer = require("./lib/Engineer");
 const Manager = require("./lib/Manager");
 
 const inquirer = require("Inquirer");
+const validator = require('email-validator');
+const multiValidator = require('validation-master');
 
 const render = require("./src/page-template");
+const cssFile = require("./src/css-contents")
 const fs = require("fs");
 const path = require("path");
 
 const OUTPUT_DIR = path.resolve(__dirname, "dist");
-const outputPath = path.join(OUTPUT_DIR, "index.html");
+const htmlPATH = path.join(OUTPUT_DIR, "index.html");
+const cssPATH = path.join(OUTPUT_DIR, "style.css");
 
 // new Manager("Luke", 1, "luke@lukepoirrier.com", "32952345");
 // new Engineer("Luke 2", 2, "lp@lukepoirrier.com", "3295");
@@ -29,6 +33,84 @@ const team = [];
 // + WHEN I decide to finish building my team
 // + THEN I exit the application, and the HTML is generated
 
+const cssContents = `
+:root{
+--gray: #223843;
+--white: #eff1f3;
+--green: #59a96a;
+--brightoj: #f79256;
+--darkoj: #d77a61;
+font-size: 16px;
+}
+body{
+  background-color: white;
+  color: white;
+}
+/* Colors for the page in the form of :root variables*/
+.green-bg{
+  background-color: var(--green);
+}
+.white-bg{
+  background-color: var(--white);
+}
+.gray-bg{
+  background-color: var(--gray);
+}
+.brightoj-bg{
+  background-color: var(--gray);
+}
+.darkoj-bg{
+  background-color: var(--gray);
+}
+.green{
+  color: var(--green);
+}
+.white{
+  color: var(--white);
+}
+.gray{
+  color: var(--gray);
+}
+.brightoj{
+  color: var(--gray);
+}
+.darkoj{
+  color: var(--gray);
+}
+
+/* End of :root variables section */ 
+
+/* Styling to make the labels look more legible and stand out against the information they represent */
+.label{
+  font-weight: bolder;
+}
+/* Overriding Bootstrap 4.3.1 stylings to add a background color and custom text color */
+.list-group-item{
+  background-color: var(--green) !important; 
+  font-weight: bold;
+}
+/* Add flex-wrap property so the cards don't continue to line up horizontally after the page is filled */
+#team-holder{
+  flex-wrap: wrap;
+}
+.team-card{
+  justify-content: space-between;
+  max-width: 18rem; 
+  display: flex;
+  flex-direction: column;
+  border: none;
+  margin: 1rem;
+}
+.team-card a{
+  color: var(--gray);
+  text-decoration: underline;
+}
+.my-team-heading{
+  color: var(--gray);
+  background-color: var(--green);
+}
+`
+
 // _ name, employee ID, email address, and office number
 function init() {
   // Inquirer Prompt for manager creation
@@ -39,39 +121,26 @@ function init() {
           type: "input",
           name: "managerName",
           message: "What is the name of the manager of your team?",
-          validate: (answer) => {
-            return true;
-            //   answer !== "" ? true : "Please enter a manager name";
-          },
+          validate: (answer) => multiValidator.isAlphabetic(answer) ? true : false
         },
         {
           type: "input",
           name: "managerId",
           message: "What is the ID of the manager of your team?",
-          validate: (answer) => {
-            if (answer.match(/^[1-9]\d*$/)) {
-              return true;
-            } else {
-              return `Please enter a valid ID number(A number greater than 0)`;
-            }
-          },
+          validate: (answer) => multiValidator.isNumeric(answer) ? true : "Please enter a valid ID Number"
         },
         {
           type: "input",
           name: "managerEmail",
           message: "What is the email of the manager of your team?",
-          validate: (answer) => answer ? true : "Please enter a valid email address"
+          validate: (answer) => validator.validate(answer) ? true : "Please enter a valid email address"
           },
         {
           type: "input",
           name: "managerOfficeNumber",
           message:
-            "What is the office phone number of the manager of your team?"
-          // validate: (answer) => {
-          //   answer.match(/^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/)
-          //     ? true
-          //     : "Please enter a phone number";
-          // },
+            "What is the office phone number of the manager of your team?",
+            validate: (answer) => multiValidator.isNumeric(answer) ? true : "Please enter a valid phone number"
         },
       ])
       .then((answers) => {
@@ -110,11 +179,11 @@ function init() {
         break;
       case "No more team members":
         console.log(team)
-        renderHTML(outputPath, team);
+        renderHTML(htmlPATH, team);
         break;
       default:
         // default
-        renderHTML(outputPath, team);
+        renderHTML(htmlPATH, team);
         break;
     }
   }))}; // </createTeam()>
@@ -135,33 +204,22 @@ function init() {
         {
           type: "input",
           name: "id",
-          message: "What is the ID of the engineer?"
-          // validate: (answer) => {
-          //   answer.match(/^[1-9]\d*$/)
-          //     ? true
-          //     : "Please enter a valid ID number(A number greater than 0)";
-          // },
+          message: "What is the ID of the engineer?",
+          validate: (answer) => multiValidator.isNumeric(answer) ? true : "Please enter a valid ID Number"
         },
         {
           type: "input",
           name: "email",
           message: "What is the email of the engineer?",
-          // validate: (answer) => {
-          //   answers.match(/[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}/gim)
-          //     ? true
-          //     : "Please enter a valid email address";
-          // },
+          validate: (answer) => validator.validate(answer) ? true : "Please enter a valid email address"
         },
         {
           type: "input",
           name: "github",
           message: "What is the GitHub username of the Engineer?",
-        //   validate: (answer) =>
-        //     answer !== ""
-        //       ? true
-        //       : "Please enter the Engineer's GitHub username",
-        },
-      ])
+          validate: (answer) => answer != null ? true : "Please enter a valid GitHub username"
+        }
+        ])
       .then((answers) => {
         const engineer = new Engineer(
           answers.name,
@@ -193,32 +251,21 @@ function init() {
           type: "input",
           name: "id",
           message: "What is the ID of the intern?",
-          // validate: (answer) => {
-          //   answer.match(/^[1-9]\d*$/)
-          //     ? true
-          //     : "Please enter a valid ID number(A number greater than 0)";
-          // },
+          validate: (answer) => multiValidator.isNumeric(answer) ? true : "Please enter a valid ID Number"
         },
         {
           type: "input",
           name: "email",
           message: "What is the email of the intern?",
-          // validate: (answer) => {
-          //   answers.match(/[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}/gim)
-          //     ? true
-          //     : "Please enter a valid email address";
-          // },
+          validate: (answer) => validator.validate(answer) ? true : "Please enter a valid email address"
         },
         {
           type: "input",
           name: "school",
           message: "What school does/did the intern attend?",
-        //   validate: (answer) =>
-        //     answer !== ""
-        //       ? true
-        //       : "Please enter the intern's alma matter",
-        },
-      ])
+          validate: (answer) => answer != "" ? true : "Please enter a valid school name"
+        }
+        ])
       .then((answers) => {
         const intern = new Intern(
           answers.name,
@@ -234,12 +281,12 @@ function init() {
   }
 
   function renderHTML(outputPath, team) {
-    // Create the output directory if the output path doesn't exist
     if (!fs.existsSync(OUTPUT_DIR)) {
       fs.mkdirSync(OUTPUT_DIR);
     }
     console.log(team);
-    fs.writeFileSync(outputPath, render(team), "utf8");
+    fs.writeFileSync(htmlPATH, render(team), "utf8");
+    fs.writeFileSync(cssPATH, cssFile(), "utf8");
   }
 } // Close the init() function
 init();
