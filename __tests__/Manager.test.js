f391e87b7cc573b13d1f2adb8882dc83e7dd2e9e
Luke Poirrier <lukepoirrier5@gmail.com>
2021-08-31 12:57:39 -0400

    Add test to ensure that the office phone number property of a Manager object can be set correctly at the time of the instantiation of the Manager object.

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -5,5 +5,10 @@ describe('Manager', () => {
       test('Should instantiate Manager object as an instance of the employee class', () => {
             const manager = new Manager();
             expect(manager).toBeInstanceOf(Employee);
+      }),
+      test('Should allow the office number property to be set at instantiation', () => {
+            const officeNumber = '1234567890';
+            const manager = new Manager("Manager Name", 1234, "Manager@email.com", officeNumber);
+            expect(manager.officeNumber).toBe(officeNumber);
       })
 })
