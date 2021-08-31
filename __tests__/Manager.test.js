9a13a8f92202876333446623b6027ce0a52f0bf0
Luke Poirrier <lukepoirrier5@gmail.com>
2021-08-31 12:56:50 -0400

    Add test to check that Manager is an instance of the Employee Class

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,3 +1,9 @@
 const Employee = require('../lib/Employee');
 const Manager = require('../lib/Manager');
 
+describe('Manager', () => {
+      test('Should instantiate Manager object as an instance of the employee class', () => {
+            const manager = new Manager();
+            expect(manager).toBeInstanceOf(Employee);
+      })
+})
